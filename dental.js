5ba611c7b767a0014d6a7a059681739f8203e8c9
gauravgupta-gsp <gauravgupta.gsp@gmail.com>
2017-12-09 08:47:18 +0530

    added 1 map 2) Updated treatments to dental services 3) Updated headers of services like orthodontics , implants , cosmetic dentistry 4) Added bootstrap classes

---
+++ dental.js
@@ -0,0 +1,11 @@
+function initMap() {
+        var uluru ={lat:31.619,lng:74.923};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
