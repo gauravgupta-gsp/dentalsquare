444e350242a0b43787d1db454ae721ef4b64bc1e
gauravgupta-gsp <gauravgupta.gsp@gmail.com>
2017-12-24 15:50:13 +0530

    Update lat long in dental.js

--- js/dental.js
+++ js/dental.js
@@ -1,7 +1,10 @@
 function initMap() {
-        var uluru ={lat:31.613705,lng:74.925880};
+        // var uluru ={lat:31.613705,lng:74.925880};
+        // 31.6125662, 74.9257597
+        var uluru ={lat:31.6125662,lng:74.9257597};
+        // 31.6535206,74.8599185
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 17,
+          zoom: 19,
           center: uluru
         });
         var marker = new google.maps.Marker({
